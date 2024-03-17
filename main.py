6235fa1ebb71973ce5affc5fcc1b8a1110fe5555
slmtml <141494318+slmtml@users.noreply.github.com>
2024-03-18 00:32:25 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+yo = {
+"LOL":"komik bir şeye verilen cevap",
+"CRINGE":"garip ya da utandırıcı bir şey",  
+"ROFL":"bir şakaya karşılık cevap",
+"SHEESH":"onaylamamak",
+"CREEPY":"korkunç",
+"AGGRO":"agresifleşmek/sinirlenmek"
+}
+for i in yo.keys():
+  print(i)
