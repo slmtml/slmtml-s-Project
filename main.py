c42796b752b7479950e4b041b74d9bfa843840e8
slmtml <141494318+slmtml@users.noreply.github.com>
2024-03-18 01:05:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 yo = {
 "LOL":"komik bir şeye verilen cevap",
 "CRINGE":"garip ya da utandırıcı bir şey",  
@@ -6,5 +7,21 @@ yo = {
 "CREEPY":"korkunç",
 "AGGRO":"agresifleşmek/sinirlenmek"
 }
+soru="a"
+ogrenilenler=[]
 for i in yo.keys():
-  print(i)
+    print(i)
+while len(ogrenilenler)!=len(yo):
+    time.sleep(0.5)
+    soru=input("\n"+"Bİlmediğiniz kelimeyi yazın --> ").upper()
+    time.sleep(0.5)
+    for i in yo.keys() :
+        if soru in i:
+            print(yo[i])
+            ogrenilenler.append(i)
+                
+
+    if soru not in yo:
+        print("Böyle bir kelime mevcut değil")
+        
+print("\n"+"Tebrikler bütün kelimeleri öğrendin.")
